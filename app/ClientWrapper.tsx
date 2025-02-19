f27d9a8b6af03857da734e852c89edb067d46931
WorldSportStars <tvkonflikt@gmail.com>
2025-02-19 04:41:41 +0100

    Client Wrapper Fix

--- app/ClientWrapper.tsx
+++ app/ClientWrapper.tsx
@@ -8,7 +8,7 @@ import '@coinbase/onchainkit/styles.css';
 export default function ClientWrapper({ children }: { children: React.ReactNode }) {
   return (
     <OnchainKitProvider 
-      apiKey="knExog65EV5TldDEwXnyikAXqB2GCAGG" 
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY} 
       chain={base}
     >
       <ThemeProvider>
