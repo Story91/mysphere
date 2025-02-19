6b71a85bfeaca4216cab11670b16a938de5a488e
WorldSportStars <tvkonflikt@gmail.com>
2025-02-19 04:47:42 +0100

    Client Wrapper Issue on Mobile Fix

---
+++ app/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Open+Sans:wght@400;700&family=Montserrat:wght@400;700&family=Lato:wght@400;700&family=Poppins:wght@400;700&family=Pacifico&family=Dancing+Script&family=Lobster&family=Permanent+Marker&family=Playfair+Display:wght@400;700&family=Merriweather:wght@400;700&family=Crimson+Text:wght@400;700&family=Bebas+Neue&family=Oswald:wght@400;700&family=Anton&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
